935b44f13cbbfe11c0e41acd8b55e8d6b291695f
Deepak Kumar <deepakdagur0007@gmail.com>
2025-08-11 10:30:32 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Header from './Header/Header.jsx'
+import Footer from './Footer/Footer.jsx'
+import Container from './container/Container.jsx'
+import Logo from './Logo.jsx'
+import LogoutBtn from './Header/LogoutBtn.jsx'
+import Button from './Button.jsx'
+import Input from './Input.jsx'
+import RTE from './RTE.jsx'
+import Signup from './Signup.jsx'
+import Login from './Login.jsx'
+import PostForm from './post-from/PostForm.jsx'
+import PostCard from './PostCard.jsx'
+import AuthLayout from './AuthLayout.jsx'
+import Select from './Select.jsx'
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    Input,
+    RTE,
+    Select,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout
+}
